334e6c252e9f253d3096e2ddb941a629a01d963a
Angelitro <amedinillaandrada@safareyes.es>
2025-01-16 12:10:15 +0100

    Empezamo fuerte

---
+++ src/main/java/com/hunt2hand/Hunt2HandApplication.java
@@ -0,0 +1,13 @@
+package com.hunt2hand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hunt2HandApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hunt2HandApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hunt2hand/Hunt2HandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hunt2hand;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hunt2HandApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
