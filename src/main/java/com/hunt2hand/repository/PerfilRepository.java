1148766245698a07404e0535dc7de9b722c693c9
Angelitro <amedinillaandrada@safareyes.es>
2025-01-29 13:59:02 +0100

    id perfilvalorado/r cojer de token

--- src/main/java/com/hunt2hand/repository/PerfilRepository.java
+++ src/main/java/com/hunt2hand/repository/PerfilRepository.java
@@ -13,9 +13,7 @@ import java.util.Optional;
 
 @Repository
 public interface PerfilRepository extends JpaRepository<Perfil, Long> {
-    List<Perfil> findByNombreLikeIgnoreCase(String nombre);
-
-    Perfil findTopByUsuario(Usuario usuario);
+    Optional<Perfil> findByNombre(String nombre);
 
     @Query("SELECT p FROM Perfil p WHERE p.usuario.username = :username")
     Optional<Perfil> findByUsuarioUsername(@Param("username") String username);
