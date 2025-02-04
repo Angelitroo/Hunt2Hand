3ed1c4d9a32a3255ce16acd3a2fb6ad62c970336
Javiermr03 <fmoyarivero@safareyes.es>
2025-02-04 11:37:53 +0100

    haciendo favs

---
+++ src/main/java/com/hunt2hand/repository/FavoritosRepository.java
@@ -0,0 +1,12 @@
+package com.hunt2hand.repository;
+
+import com.hunt2hand.model.Favoritos;
+import com.hunt2hand.model.Perfil;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface FavoritosRepository extends JpaRepository<Favoritos, Long> {
+    List<Favoritos> findByPerfil(Perfil perfil);
+
+}
---
+++ src/main/java/com/hunt2hand/service/FavoritosService.java
@@ -0,0 +1,42 @@
+package com.hunt2hand.service;
+
+import com.hunt2hand.model.Favoritos;
+import com.hunt2hand.model.Perfil;
+import com.hunt2hand.model.Producto;
+import com.hunt2hand.repository.FavoritosRepository;
+import com.hunt2hand.repository.PerfilRepository;
+import com.hunt2hand.repository.ProductoRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+@Validated
+public class FavoritosService {
+
+    private final FavoritosRepository favoritosRepository;
+    private final PerfilRepository perfilRepository;
+    private final ProductoRepository productoRepository;
+
+    public Favoritos addProductoToFavoritos(Long idPerfil, Long idProducto) {
+        Perfil perfil = perfilRepository.findById(idPerfil)
+                .orElseThrow(() -> new RuntimeException("Perfil con id " + idPerfil + " no encontrado"));
+        Producto producto = productoRepository.findById(idProducto)
+                .orElseThrow(() -> new RuntimeException("Producto con id " + idProducto + " no encontrado"));
+
+        Favoritos favoritos = new Favoritos();
+        favoritos.setPerfil(perfil);
+        favoritos.setProducto(producto);
+
+        return favoritosRepository.save(favoritos);
+    }
+
+    public List<Favoritos> getFavoritosByPerfil(Long idPerfil) {
+        Perfil perfil = perfilRepository.findById(idPerfil)
+                .orElseThrow(() -> new RuntimeException("Perfil con id " + idPerfil + " no encontrado"));
+        return favoritosRepository.findByPerfil(perfil);
+    }
+}
