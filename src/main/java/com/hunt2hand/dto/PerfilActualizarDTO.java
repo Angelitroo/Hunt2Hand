78ab013ccaf44767082cbcc823d03d13dcb10907
Angelitro <amedinillaandrada@safareyes.es>
2025-02-04 13:46:25 +0100

    Modificar perfil hecho

---
+++ src/main/java/com/hunt2hand/dto/PerfilActualizarDTO.java
@@ -0,0 +1,14 @@
+package com.hunt2hand.dto;
+
+import lombok.Data;
+
+@Data
+public class PerfilActualizarDTO {
+    private Long id;
+    private String nombre;
+    private String apellido;
+    private String ubicacion;
+    private String imagen;
+    private String username;
+    private String password;
+}
