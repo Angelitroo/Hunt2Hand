b16268880453384b08d9133dccae07b6fcbde272
alvaritosr <asanchezrubio@safareyes.es>
2025-02-12 13:49:09 +0100

    QUE EMPALO?

--- src/main/java/com/hunt2hand/controller/UsuarioController.java
+++ src/main/java/com/hunt2hand/controller/UsuarioController.java
@@ -1,4 +1,20 @@
 package com.hunt2hand.controller;
 
+import com.hunt2hand.model.Usuario;
+import com.hunt2hand.service.UsuarioService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/usuarios")
+@RequiredArgsConstructor
 public class UsuarioController {
+    private final UsuarioService usuarioService;
+
+    @GetMapping("/{id}")
+    public Usuario obtenerUsuarioPorId(Long id) {
+        return usuarioService.obtenerUsuarioPorId(id);
+    }
 }
