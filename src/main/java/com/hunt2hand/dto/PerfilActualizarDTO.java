370118765894f6e7647f4794df90cdf9a03d47e8
alvaritosr <asanchezrubio@safareyes.es>
2025-02-19 10:46:24 +0100

    Final

--- src/main/java/com/hunt2hand/dto/PerfilActualizarDTO.java
+++ src/main/java/com/hunt2hand/dto/PerfilActualizarDTO.java
@@ -12,4 +12,68 @@ public class PerfilActualizarDTO {
     private String email;
     private String username;
     private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
--- src/main/java/com/hunt2hand/dto/PerfilDTO.java
+++ src/main/java/com/hunt2hand/dto/PerfilDTO.java
@@ -12,4 +12,68 @@ public class PerfilDTO {
     private Boolean activado;
     private Boolean baneado;
     private Long Usuario;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+
+    public Boolean getActivado() {
+        return activado;
+    }
+
+    public void setActivado(Boolean activado) {
+        this.activado = activado;
+    }
+
+    public Boolean getBaneado() {
+        return baneado;
+    }
+
+    public void setBaneado(Boolean baneado) {
+        this.baneado = baneado;
+    }
+
+    public Long getUsuario() {
+        return Usuario;
+    }
+
+    public void setUsuario(Long usuario) {
+        Usuario = usuario;
+    }
 }
